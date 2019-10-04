b545935ae0711a6c514fa1d04b588451b2c3e05c
mattRedBox <matt@redboxresearchdata.com.au>
2019-10-04 13:40:43 +1000

    Updated docker build details for branch so commit/master/travis_br aren't committed.

--- dockerhub_deploy.sh
+++ dockerhub_deploy.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-export REPO=qcifengineering/redbox-portal
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH; fi`
+export REPO=qcifengineering/redbox-portal:sails-hook-redbox-pdfgen
 yarn install;
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker build -f Dockerfile -t $REPO .
 docker push $REPO
